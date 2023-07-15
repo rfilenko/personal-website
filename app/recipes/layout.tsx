044992ae44d20ca252464cc304613de1424ef08f
Roman Filenko <roman.filenko@giant.cz>
2023-07-15 07:22:50 +0200

    added coming soon page

--- app/recipes/layout.tsx
+++ app/recipes/layout.tsx
@@ -1,10 +1,15 @@
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import { Providers } from "./components/Providers";
-import "./globals.css";
+import Footer from "@/app/components/Footer";
+import Navbar from "@/app/components/Navbar";
+import { Providers } from "@/app/components/Providers";
+import "@/app/globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "HomeApp",
+  description: "Simple usefull HomeApp",
+};
 
 export default function RootLayout({
   children,
@@ -17,10 +22,11 @@ export default function RootLayout({
         className={` ${inter.className} bg-white text-black dark:bg-gray-900 dark:selection:bg-gray-900 dark:text-white h-full selection:bg-gray-50`}
       >
         <Providers>
-          <main className="relative flex flex-col items-center justify-center w-full h-full overflow-hidden bg-gradient-to-r from-gray-850 to-gray-900 ">
-            {/* <main className="max-w-5xl mx-auto relative p-4 sm:p-8 lg:p-16 lg:pt-2 overflow-hidden"> */}
+          <Navbar />
+          <main className="max-w-5xl mx-auto relative p-4 sm:p-8 lg:p-16 lg:pt-2 overflow-hidden">
             {children}
           </main>
+          <Footer />
         </Providers>
       </body>
     </html>
---
+++ app/recipes/page.tsx
@@ -0,0 +1,69 @@
+import { PortableText } from '@portabletext/react';
+import { Recipe } from '@/app/lib/interface';
+import { getRecipeData } from '@/app/lib/sanity';
+import Link from 'next/link';
+import { urlFor } from '@/app/lib/sanityImageUrl';
+import Image from 'next/image';
+
+export const metadata = {
+    title: "Recipes | HomeApp",
+    description: "List of available recipes",
+};
+
+export default async function Home() {
+    const data = await getRecipeData() as Recipe[]
+
+    const PortableTextComponent = {
+        types: {
+            image: ({ value }: { value: any }) => (
+                <Image
+                    src={urlFor(value).url()}
+                    alt="Image"
+                    className="rounded-lg absolute w-full h-full object-cover"
+                    width={175}
+                    height={175}
+                />
+            ),
+        },
+    };
+
+    return (
+        <div className="">
+            <div className='font-semibold flex gap-2 justify-between items-center'>
+                <h1 className='my-2 text-3xl'>List of recipes</h1>
+                <div className='text-2xl'>Total: <span>{data.length} recipes</span></div>
+            </div>
+
+            <ul className='recipe-list mt-4'>
+                {data.map((recipe) => (
+
+                    <li key={recipe._id} className="recipe-item p-4">
+                        <Link
+                            href={`/recipe/${recipe.slug.current}`}
+                            prefetch
+                            className="space-y-3 xl:col-span-3 h-full flex flex-col justify-between relative"
+                        >
+                            <div className='flex gap-4 justify-between'>
+                                <h3 className="text-xl font-bold leading-6 tracking-tight text-gray-900 dark:text-gray-100">
+                                    {recipe.title}
+                                </h3>
+                                <span className='text-white right-0 text-xs font-bold bg-red-500 p-1 rounded self-start'>{recipe.categories[0]}</span>
+                            </div>
+
+                            <div className="recipe-image relative overflow-hidden h-[150px] shrink-0 grow-3">
+                                <PortableText
+                                    value={recipe.content}
+                                    components={PortableTextComponent}
+                                />
+                            </div>
+
+                            <p className="prose max-w-none text-gray-500 dark:text-gray-400 line-clamp-2 mt-auto leading-6">
+                                {recipe.overview}
+                            </p>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
