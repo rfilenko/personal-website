38b4c9757ef1daf434ab3bb40f0cb2b99254fd65
Roman Filenko <roman.filenko@giant.cz>
2023-07-17 22:02:17 +0200

    delete notes page, added gradient coming soon

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export const metadata = {
 	title: "Coming soon page",
 	description: "Project is loading...",
@@ -7,16 +5,15 @@ export const metadata = {
 
 export default async function Home() {
 	return (
-		<>
-			<div className="bg-visual absolute w-full h-full inset-0">
-				<Image src="/gradient.jpg" alt="visual" fill sizes='100vw' />
-			</div>
+		<div className="bg-visual" style={{
+			'background': 'radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(38,122,78,1) 73%)'
+		}}>
 			<main className="relative flex flex-col items-center justify-center w-full h-full overflow-hidden">
 				<div className='font-semibold flex flex-col gap-2 justify-between items-center'>
 					<h1 className='my-2 font-sans text-4xl md:text-6xl text-white'>Coming soon</h1>
-					<div className='text-base text-secondary text-gray-400'>Something is coming up... Stay tuned</div>
+					<div className='text-base text-gray-100'>Something is coming up... Stay tuned</div>
 				</div>
 			</main>
-		</>
+		</div>
 	)
 }
