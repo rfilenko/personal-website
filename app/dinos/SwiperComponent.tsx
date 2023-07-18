936d79b0940b3649984e9eedab2133ef8949196a
Roman Filenko <roman.filenko@giant.cz>
2023-07-18 05:49:24 +0200

    shuffle dinos on page load

--- app/dinos/SwiperComponent.tsx
+++ app/dinos/SwiperComponent.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import ReactAudioPlayer from 'react-audio-player';
 import { Dino } from "@/app/lib/interface";
@@ -13,16 +13,7 @@ import 'swiper/css/scrollbar';
 import { Scrollbar } from 'swiper/modules';
 
 export default function SwiperComponent({ children, dinoData }: { children?: React.ReactNode, dinoData: Dino[] }) {
-    function shuffleArray(array: Dino[]) {
-        const arr = array.sort(() => .5 - Math.random())
-        return arr
-    }
-
     const [dinosArr, setDinosArr] = useState<Dino[]>(dinoData)
-
-    useEffect(() => {
-        setDinosArr(shuffleArray(dinoData))
-    }, [dinosArr])
 
     return <>
         <Swiper
@@ -30,25 +21,22 @@ export default function SwiperComponent({ children, dinoData }: { children?: Rea
         modules={[Scrollbar]}
         spaceBetween={25}
         className="swiperWrap"
-        style={{ width: '100%', height: '100%' }}
+            style={{ width: '100%', height: '100%' }} 
     >
         {children}
             {dinosArr && dinosArr.map((dino: Dino) => {
-            const dinoSlug = dino.name.toLowerCase()
-            const dinoImgUrl = `/dino/${dinoSlug}.png`
-                const hasAudioFIle = `dino/${dinoSlug}.mp3`.length
+                const dinoSlug = dino.name.toLowerCase()
+                const dinoImgUrl = `/dino/${dinoSlug}.png`
 
             return <SwiperSlide style={{ minHeight: '100%' }} key={dino._id}>
                 <div className={`border-2 border-${dino.color}-400 dino m-4 mx-auto p-4 px-4 rounded-md max-w-lg`}>
                     <header className="flex flex-col gap-2 items-center mb-4">
                         <h2 className="font-extrabold text-2xl">{dino.name}</h2>
-
-                        {hasAudioFIle && (
-                            <ReactAudioPlayer
+                        {(dinoSlug == 'archaeopteryx' || dinoSlug == 'triceratops' || dinoSlug == 't-rex') && <ReactAudioPlayer
                             src={`dino/${dinoSlug}.mp3`} controls
                             className="block md:w-[300px]"
-                            />
-                        )}
+                        />
+                        }
                     </header>
                     <div className="relative flex justify-center">
                         <span className={`w-[200px] h-[200px] bg-${dino.color}-700 absolute -z-0 rounded-full border-4 border-gray-300`}></span>
@@ -56,9 +44,10 @@ export default function SwiperComponent({ children, dinoData }: { children?: Rea
                     </div>
 
                     <div className="dino-info flex flex-col gap-1 mt-4">
-                        <h3 className={`font-bold text-xl text-${dino.color}-400`}>Info:</h3>
+                        <h3 className={`font-bold text-xl text-${dino.color}-300`}>Info:</h3>
                         <p className="flex gap-1 items-center"><span className="font-bold">Diet:</span> {dino.diet}
-                            <TbMeat style={{ color: dino.color }} size={30} /></p>
+                            {(dino.color == 'green') ? <TbLeaf style={{ color: dino.color }} size={30} /> : (dino.color == 'red') ? <TbMeat style={{ color: dino.color }} size={30} /> : (dino.color == 'blue') ? <TbLeaf style={{ color: dino.color }} size={30} /> : ""}
+                        </p>
                         <p><span className="font-bold">Type of dinosaur:</span>{dino.type}</p>
                         <p className="flex gap-2">
                             {dino.dimensions}
--- app/dinos/page.tsx
+++ app/dinos/page.tsx
@@ -6,11 +6,17 @@ import SwiperComponent from "./SwiperComponent";
 export default async function DinoPage() {
     const dinoData = await getDinoData() as Dino[]
 
+    function shuffleArray(array: Dino[]) {
+        const arr = array.sort(() => .5 - Math.random())
+        return arr
+    }
+    const shuffledDinoData = shuffleArray(dinoData)
+
     return <>
         <h2 className="mt-4 font-extrabold text-3xl text-center text-teal-400">Dinosaurs</h2>
 
         <div className="px-4 md:pt-8">
-            <SwiperComponent dinoData={dinoData} />
+            <SwiperComponent dinoData={shuffledDinoData} />
         </div>
     </>
 }
