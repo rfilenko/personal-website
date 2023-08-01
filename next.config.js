2d572356f06f38af5735e7c50ea39a3e814ddd17
Roman Filenko <roman.filenko@giant.cz>
2023-08-01 11:24:14 +0200

    dev changes next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // fastRefresh: true,
+    // concurrentFeatures: true,
+    productionBrowserSourceMaps: false,
+    optimizeFonts: false,
+    minify: false,
     images: {
         domains: ["cdn.sanity.io"],
     },
