d8c5e61818113daca4ca091c90d706f8a1ce653d
Roman Filenko <roman.filenko@gmail.com>
2024-08-12 12:44:57 +0200

    feat(updates): add recent job, update  description

--- app/components/resume/SectionExperience.tsx
+++ app/components/resume/SectionExperience.tsx
@@ -1,44 +1,55 @@
-'use client'
+"use client";
 
-import { Job } from '@/app/lib/interface';
-import { motion } from 'framer-motion';
+import { Job } from "@/app/lib/interface";
+import { motion } from "framer-motion";
 
-import JobCard from './JobCard';
-import ResumeSection from './ResumeSection'
+import JobCard from "./JobCard";
+import ResumeSection from "./ResumeSection";
 
 export default function SectionExperience({ jobData }: { jobData: Job[] }) {
-    const wrap = {
-        hidden: {
-            scale: .8,
-            opacity: 0,
-            transition: { when: "beforeChildren", type: "easeIn" }
-        },
-        show: {
-            scale: 1,
-            opacity: 1,
-            transition: {
-                staggerChildren: 0.25
-            },
-        },
-    }
-    const jobVariant = {
-        hidden: {
-            opacity: 0
-        },
-        show: {
-            opacity: 1,
-        },
-    }
+	const wrap = {
+		hidden: {
+			scale: 0.8,
+			opacity: 0,
+			transition: { when: "beforeChildren", type: "easeIn" }
+		},
+		show: {
+			scale: 1,
+			opacity: 1,
+			transition: {
+				staggerChildren: 0.25
+			}
+		}
+	};
+	const jobVariant = {
+		hidden: {
+			opacity: 0
+		},
+		show: {
+			opacity: 1
+		}
+	};
 
-    return <>
-        <ResumeSection sectionId="section-experience">
-            <motion.div key="experience" initial="hidden" animate="show" variants={wrap}>
-                <h2 className='mb-2 md:mb-8 text-2xl font-sans font-semibold'>Experience</h2>
+	return (
+		<>
+			<ResumeSection sectionId="section-experience">
+				<motion.div
+					key="experience"
+					initial="hidden"
+					animate="show"
+					variants={wrap}
+				>
+					<h2 className="mb-2 md:mb-8 text-2xl font-sans font-semibold">
+						Experience
+					</h2>
 
-                <div className='grid sm:grid-cols-2 sm:gap-8 md:gap-12'>
-                    {jobData.map(job => <JobCard job={job} key={job.company} jobVariant={jobVariant} />)}
-                </div>
-            </motion.div>
-        </ResumeSection>
-    </>
+					<div className="grid sm:grid-cols-2 sm:gap-8 md:gap-12">
+						{jobData.map((job) => (
+							<JobCard job={job} key={job.company} jobVariant={jobVariant} />
+						))}
+					</div>
+				</motion.div>
+			</ResumeSection>
+		</>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,42 @@
-import { getJobData, getProjectData } from '@/app/lib/sanity';
-import { Job, Project } from '@/app/lib/interface';
+import { getJobData, getProjectData } from "@/app/lib/sanity";
+import { Job, Project } from "@/app/lib/interface";
 
-const position = `</FrontendDeveloper>`
+const position = `</FrontendDeveloper>`;
 export const metadata = {
 	title: `Roman Filenko-${position}`,
-	description: "Simple usefull HomeApp",
+	description:
+		"Explore my personal portfolio showcasing my professional experience in web development,"
 };
 
-import ResumeSection from '@/app/components/resume/ResumeSection'
+import ResumeSection from "@/app/components/resume/ResumeSection";
 import ResumeHeader from "@/app/components/resume/ResumeHeader";
-import ResumeFooter from '@/app/components/resume/ResumeFooter';
-import ResumeIntro from '@/app/components/resume/ResumeIntro';
-import SectionExperience from '@/app/components/resume/SectionExperience';
-import SectionProjects from '@/app/components/resume/SectionProjects';
-import SectionContact from './components/resume/SectionContact';
+import ResumeFooter from "@/app/components/resume/ResumeFooter";
+import ResumeIntro from "@/app/components/resume/ResumeIntro";
+import SectionExperience from "@/app/components/resume/SectionExperience";
+import SectionProjects from "@/app/components/resume/SectionProjects";
+import SectionContact from "./components/resume/SectionContact";
 
 export default async function Home() {
-	const jobData = await getJobData() as Job[]
-	const projectData = await getProjectData() as Project[]
+	const jobData = (await getJobData()) as Job[];
+	const projectData = (await getProjectData()) as Project[];
 
 	return (
 		<div className="bg-slate-900 h-full selection:bg-teal-600 selection:text-gray-100">
-			<div className="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute" style={{ background: 'radial-gradient(600px at 1008px 241px, rgba(29, 78, 216, 0.15), transparent 80%)' }}></div>
-			<div className='md:max-w-5xl mx-auto relative h-full w-full py-2 md:py-8 px-4 md:px-8 text-white overflow-x-hidden'>
+			<div
+				className="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute"
+				style={{
+					background:
+						"radial-gradient(600px at 1008px 241px, rgba(29, 78, 216, 0.15), transparent 80%)"
+				}}
+			></div>
+			<div className="md:max-w-5xl mx-auto relative h-full w-full py-2 md:py-8 px-4 md:px-8 text-white overflow-x-hidden">
 				<ResumeHeader />
 
 				<main>
-					<ResumeSection sectionId="section-about" className="about relative grid md:grid-cols-2 place-items-center py-4 px-2 md:px-16 min-h-[24rem]">
+					<ResumeSection
+						sectionId="section-about"
+						className="about relative grid md:grid-cols-2 place-items-center py-4 px-2 md:px-16 min-h-[24rem]"
+					>
 						<ResumeIntro />
 					</ResumeSection>
 
@@ -41,5 +51,5 @@ export default async function Home() {
 				<ResumeFooter />
 			</div>
 		</div>
-	)
+	);
 }
